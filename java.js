92c2fd54c44ae09788d48182adf468c4587edc84
A.J. Fraser <ajamiesonfraser@gmail.com>
2014-10-03 16:44:09 -0300

    my first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+//A.J. Fraser's Chat User Interface
+console.log("script loaded")
+
+function mySubmit() {
+  var message = document.getElementById("texttosend").value;
+  console.log('This is Message: '+ message);
+  document.getElementById("chatlog").appendChild(document.createTextNode(message));
+  document.getElementById("chatlog").appendChild(document.createElement("br"));
+  document.getElementById("texttosend").value = "";
+}
